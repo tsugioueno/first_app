eba593fe22658ab9e3957aff0a395ce40d6c1db8
tsugioueno <fmi4bm@icloud.com>
2020-09-01 18:25:17 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all  # すべてのレコードを@postsに代入
+  end
+  def new
+
+    def create
+      Post.create(content: params[:content])
+    end
+  end
+end
